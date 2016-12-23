39a3299e194400d3db4d70ccae02e1c9acb4fc19
Matthew Preucil <mattpsbs@gmail.com>
2016-12-23 13:40:35 -0800

    Fix mapping streams.

--- Sources/map.swift
+++ Sources/map.swift
@@ -1,39 +1,46 @@
-public typealias SingleValueMapper<S, T> = (S) -> T
+public typealias OneToOneMapping<S, T> = (S) -> T
 
-public typealias MultipleValueMapper<S, T> = ([S]) -> (T?, [S])
+public typealias ManyToOneMapping<S, T> = ([S]) -> (T?, [S])
+
+public typealias OneToManyMapping<S, T> = (S) -> [T]
 
 public extension IReadableStream
 {
-	public func map<T: IReadableStream>(_ mapper: @escaping SingleValueMapper<ChunkType, T.ChunkType>) -> T {
-		let s = SingleValueMappingStream(self, mapper)
+	public func map<T>(_ mapping: @escaping OneToOneMapping<ChunkType, T>) -> ReadableStream<T> {
+		let mapped: Stream<T> = Stream()
 		_ = self.subscribe { data in
-			s.publish(mapper(data))
+			mapped.publish(mapping(data))
 		}
-		return s as! T
+		return ReadableStream(mapped)
 	}
 
-	public func map<T: IReadableStream>(_ mapper: @escaping MultipleValueMapper<ChunkType, T.ChunkType>) -> T {
-		let s = MultipleValueMappingStream(self, mapper)
+	public func map<T>(_ mapping: @escaping ManyToOneMapping<ChunkType, T>) -> ReadableStream<T> {
+		let mapped = ManyToOneMappingStream(mapping)
 		_ = self.subscribe { data in
-			s.accumulate(data)
+			mapped.accumulate(data)
 		}
-		return s as! T
+		return ReadableStream(mapped)
+	}
+
+	public func map<T>(_ mapping: @escaping OneToManyMapping<ChunkType, T>) -> ReadableStream<T> {
+		let mapped: Stream<T> = Stream()
+		_ = self.subscribe { data in
+			let many = mapping(data)
+			for item in many {
+				mapped.publish(item)
+			}
+		}
+		return ReadableStream(mapped)
 	}
 }
 
-internal class SingleValueMappingStream<S: IReadableStream, T> : Stream<T>
+internal class ManyToOneMappingStream<S, T> : Stream<T>
 {
-	internal init(_ source: S, _ map: @escaping SingleValueMapper<S.ChunkType, T>) {
-	}
-}
-
-internal class MultipleValueMappingStream<S: IReadableStream, T> : Stream<T>
-{
-	internal init(_ source: S, _ map: @escaping MultipleValueMapper<S.ChunkType, T>) {
+	internal init(_ map: @escaping ManyToOneMapping<S, T>) {
 		_map = map
 	}
 
-	internal func accumulate(_ data: S.ChunkType) {
+	internal func accumulate(_ data: S) {
 		_accumulator.append(data)
 		let (result, leftovers) = _map(_accumulator)
 		_accumulator = leftovers
@@ -42,6 +49,6 @@ internal class MultipleValueMappingStream<S: IReadableStream, T> : Stream<T>
 		}
 	}
 
-	var _accumulator: [S.ChunkType] = []
-	var _map: MultipleValueMapper<S.ChunkType, T>
+	var _accumulator: [S] = []
+	var _map: ManyToOneMapping<S, T>
 }
