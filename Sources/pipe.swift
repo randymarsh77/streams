64272400b9519a2d151c64367083ebd6c1d24f2e
Matthew Preucil <mattpsbs@gmail.com>
2017-01-22 16:17:11 -0800

    Make pipe chainable.

--- Sources/pipe.swift
+++ Sources/pipe.swift
@@ -1,17 +1,19 @@
 public extension IReadableStream
 {
-	public func pipe<S: IWriteableStream>(to: S) where S.ChunkType == Self.ChunkType {
+	public func pipe<S: IWriteableStream, T: IReadableStream>(to: S) -> T where S.ChunkType == Self.ChunkType, T.ChunkType == Self.ChunkType {
 		_ = self.subscribe { chunk in
 			to.publish(chunk)
 		}
+		return self as! T
 	}
 }
 
 public extension IWriteableStream
 {
-	public func pipe<S: IReadableStream>(from: S) where S.ChunkType == Self.ChunkType {
+	public func pipe<S: IReadableStream, T: IWriteableStream>(from: S) -> T where S.ChunkType == Self.ChunkType, T.ChunkType == Self.ChunkType {
 		_ = from.subscribe { chunk in
 			self.publish(chunk)
 		}
+		return self as! T
 	}
 }
